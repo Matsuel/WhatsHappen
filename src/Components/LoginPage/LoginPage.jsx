c6efd4167b45a5bdedfe10502e3f5423704c72a6
Matsuel <matsuel0308@gmail.com>
2023-11-15 14:43:24 +0100

    login

--- src/Components/LoginPage/LoginPage.jsx
+++ src/Components/LoginPage/LoginPage.jsx
@@ -1,16 +1,34 @@
-import React from 'react'
+import React,{useState} from 'react'
 import './LoginPage.css'
 import WhatsApp from '../../assets/whatsapp.svg'
+import axios from 'axios'
 
 const LoginPage = () => {
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios.post('http://localhost:3001/validatePassword', {email, password})
+    .then(res => {
+      if(res.data.validation){
+        alert('Vous êtes connecté')
+      }else{
+        alert('Vous n\'êtes pas connecté')
+      }
+    })
+  }
+
   return (
     <div className='LoginPage'>
       <div className="login">
-        <form method='post' action='/login' className='formClass'>
+        <form className='formClass'>
         <h1 className='title'> <img src={WhatsApp} width={40} className='logo'/>  Connexion</h1>
-          <input placeholder='Entrez votre adresse mail' className='input' type="text" maxLength={20} />
-          <input placeholder='Entrez votre mot de passe' className='input' type="password" minLength={5} />
-          <button className='button'>Se connecter à WhatsApp</button>
+          <input onChange={(e)=>setEmail(e.target.value)} placeholder='Entrez votre adresse mail' className='input' type="text" maxLength={20} />
+          <input onChange={(e)=>setPassword(e.target.value)} placeholder='Entrez votre mot de passe' className='input' type="password" minLength={5} />
+          <button onClick={(e)=>handleSubmit(e)} className='button'>Se connecter à WhatsApp</button>
         </form>
       </div>
     </div>
