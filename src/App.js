deb9bfe35a2588eedcb2d7e2a2c4084856f5df90
Matsuel <matsuel0308@gmail.com>
2023-11-15 16:25:25 +0100

    add routes

--- src/App.js
+++ src/App.js
@@ -2,11 +2,18 @@ import './App.css';
 import WelcomePage from './Components/WelcomePage/WelcomePage.tsx';
 import SingUpPage from './Components/SignUpPage/SignUpPage.tsx';
 import LoginPage from './Components/LoginPage/LoginPage.tsx';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <LoginPage />
+      <Router>
+        <Routes>
+          <Route path="/" element={<WelcomePage />} />
+          <Route path="/signup" element={<SingUpPage />} />
+          <Route path="/login" element={<LoginPage />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
