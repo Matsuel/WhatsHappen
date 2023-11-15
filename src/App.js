a7ee2add89c30e5b9d0d04a2e8a24c20a5fcb3df
Matsuel <matsuel0308@gmail.com>
2023-11-15 10:24:11 +0100

    WelcomePage

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import WelcomePage from './Components/WelcomePage/WelcomePage';
 
 function App() {
   return (
     <div className="App">
-
+      <WelcomePage />
     </div>
   );
 }
