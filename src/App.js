85cbe9aacd68a90425ce80a674a019f3eed6cb02
Matsuel <matsuel0308@gmail.com>
2023-11-15 11:49:54 +0100

    signup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 import WelcomePage from './Components/WelcomePage/WelcomePage';
+import SingUpPage from './Components/SignUpPage/SignUpPage';
 
 function App() {
   return (
     <div className="App">
-      <WelcomePage />
+      <SingUpPage />
     </div>
   );
 }
