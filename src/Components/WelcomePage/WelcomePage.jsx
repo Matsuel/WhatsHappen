27788f16861a7037fb692074c8104273c4be20b0
Matsuel <matsuel0308@gmail.com>
2023-11-15 10:31:44 +0100

    WelcomePage

--- src/Components/WelcomePage/WelcomePage.jsx
+++ src/Components/WelcomePage/WelcomePage.jsx
@@ -8,7 +8,7 @@ const WelcomePage = () => {
         <h1 className='title'>Bienvenue sur WhatsApp</h1>
         <h2 className='subTitle'>Un moyen simple, fiable et confidentiel d'utiliser WhastApp sur votre ordinateur</h2>
         <button className='button'>Démarrer</button>
-        <h2 className='version'>v0.1</h2>
+        <h2 className='version'>v0.0.0.1</h2>
       </div>
     </div>
   )
