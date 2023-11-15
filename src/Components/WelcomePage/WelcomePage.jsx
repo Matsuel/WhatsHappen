49be0fe53179c08e2b34f69d3eea8271b7955a0f
Matsuel <matsuel0308@gmail.com>
2023-11-15 10:24:02 +0100

    WelcomePage

---
+++ src/Components/WelcomePage/WelcomePage.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './WelcomePage.css'
+
+const WelcomePage = () => {
+  return (
+    <div className='welcomePage'>
+      <div className="titleDiv">
+        <h1 className='title'>Bienvenue sur WhatsApp</h1>
+        <h2 className='subTitle'>Un moyen simple, fiable et confidentiel d'utiliser WhastApp sur votre ordinateur</h2>
+        <button className='button'>Démarrer</button>
+        <h2 className='version'>v0.1</h2>
+      </div>
+    </div>
+  )
+}
+
+export default WelcomePage
