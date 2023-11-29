5fdd9374c6aec531e15b3d3c05e0ee76d85c2b5c
Matsuel <matsuel0308@gmail.com>
2023-11-29 15:45:06 +0100

    add conv

--- src/Components/Home/Home.tsx
+++ src/Components/Home/Home.tsx
@@ -8,6 +8,102 @@ import Profile from '../../assets/Profile.svg'
 import Dots from '../../assets/Dots.svg'
 //@ts-ignore
 import Archive from '../../assets/Archive.svg'
+//@ts-ignore
+import DoubleTick from '../../assets/DoubleTick.svg'
+
+interface conversation {
+    id: number,
+    name: string,
+    avatar: string,
+    lastMessage: string,
+    lastMessageHour: string,
+    isLastMessageReceivedRead: boolean,
+    isLastMessageSendRead: boolean,
+    isArchived: boolean,
+    isPin: boolean,
+    messageType: string,
+    messages:[],
+    numberOfUnreadMessages: number
+}
+
+interface conversationList {
+    conversations: conversation[]
+}
+
+const conversationList: conversationList = {
+    conversations: [
+        {
+            id: 1,
+            name: 'Groupe 1',
+            avatar: '',
+            lastMessage: 'Salut',
+            lastMessageHour: '12:00',
+            isLastMessageReceivedRead: true,
+            isLastMessageSendRead: true,
+            isArchived: false,
+            isPin: false,
+            messageType: 'text',
+            messages:[],
+            numberOfUnreadMessages: 0
+        },
+        {
+            id: 2,
+            name: 'Groupe 2',
+            avatar: '',
+            lastMessage: 'Salut',
+            lastMessageHour: '12:00',
+            isLastMessageReceivedRead: true,
+            isLastMessageSendRead: true,
+            isArchived: false,
+            isPin: false,
+            messageType: 'text',
+            messages:[],
+            numberOfUnreadMessages: 0
+        },
+        {
+            id: 3,
+            name: 'Groupe 3',
+            avatar: '',
+            lastMessage: 'Salut',
+            lastMessageHour: '12:00',
+            isLastMessageReceivedRead: true,
+            isLastMessageSendRead: true,
+            isArchived: false,
+            isPin: false,
+            messageType: 'text',
+            messages:[],
+            numberOfUnreadMessages: 5
+        },
+        {
+            id: 4,
+            name: 'Groupe 4',
+            avatar: '',
+            lastMessage: 'Salut',
+            lastMessageHour: '12:00',
+            isLastMessageReceivedRead: true,
+            isLastMessageSendRead: true,
+            isArchived: false,
+            isPin: false,
+            messageType: 'text',
+            messages:[],
+            numberOfUnreadMessages: 1
+        },
+        {
+            id: 5,
+            name: 'Groupe 5',
+            avatar: '',
+            lastMessage: 'Salut',
+            lastMessageHour: '12:00',
+            isLastMessageReceivedRead: true,
+            isLastMessageSendRead: true,
+            isArchived: false,
+            isPin: false,
+            messageType: 'text',
+            messages:[],
+            numberOfUnreadMessages: 7
+        }
+    ]
+}
 
 const Home = () => {
     const [search, setSearch] = useState('')
@@ -43,6 +139,49 @@ const Home = () => {
                     <img src={Archive} alt="Archive" className='archive-logo' />
                     <h1 className='archive-title'>Archivées</h1>
                     <h1 className='archive-count'>{countArchived}</h1>
+                </div>
+                <div className="conv-list">
+                    {conversationList.conversations.map((conversation: conversation) => (
+                        <div className="conv" key={conversation.id}>
+                            <div className="avatar-conv">
+                                <img src={Profile} alt="Avatar" className="avatar" />
+                            </div>
+                            <div className="conv-details">
+                                <div className="conv-top-details">
+                                    <h1 className="conv-name">{conversation.name}</h1>
+                                    {conversation.isLastMessageReceivedRead  ? (
+                                        <h1 className="conv-hour">{conversation.lastMessageHour}</h1>
+                                    ) : (
+                                        <h1 className="conv-hour-unread">{conversation.lastMessageHour}</h1>
+                                    )}
+                                    <h1 className="conv-hour">{conversation.lastMessageHour}</h1>
+                                </div>
+                                <div className="conv-bottom-details">
+                                        {conversation.isLastMessageSendRead ? (
+                                            <div className="lastMessageUnread">
+                                                <img src={DoubleTick} alt="DoubleTick" className="doubleTick" />
+                                                <h1 className="lastMessage">{conversation.lastMessage}</h1>
+                                            </div>
+                                        ) : (
+                                            <div className="lastMessageRead">
+                                                <h1 className="lastMessage">{conversation.lastMessage}</h1>
+                                            </div>
+                                            
+                                        )}
+
+                                    {conversation.numberOfUnreadMessages > 0 ? (
+                                        <div className="numberOfUnreadMessages">
+                                            <h1 className="numberOfUnreadMessages">{conversation.numberOfUnreadMessages}</h1>
+                                        </div>
+                                    ) : (
+                                        <div className="">
+                                            
+                                        </div>
+                                    )}
+                                </div>
+                            </div>
+                        </div>
+                    ))}
                 </div>
             </div>
             <div className="converstion-active">
