7d294d5802598cd5d72b13a1faa4e8509917abbc
Matsuel <matsuel0308@gmail.com>
2024-02-07 11:25:31 +0100

    add list of users with start style

--- src/Components/Home/Home.tsx
+++ src/Components/Home/Home.tsx
@@ -13,13 +13,20 @@ interface ConversationInfos {
     name : string
 }
 
+interface UserInfos {
+    username: string,
+    _id: string
+}
+
 const Home = () => {
     const [conversations, setConversations] = useState<ConversationInfos[]>([])
     const [conversationActive, setConversationActive] = useState<string>('')
     const [search, setSearch] = useState<string>('')
+    const [searchUsers, setSearchUsers] = useState<string>('')
     const [typeConv, setTypeConv] = useState<number>(1)
     const [showNewConv, setShowNewConv] = useState<boolean>(false)
     const [canRotate, setCanRotate] = useState<boolean>(false)
+    const [users , setUsers] = useState<UserInfos[]>([])
 
     const cookies = Cookies.get('user')
     
@@ -27,6 +34,14 @@ const Home = () => {
         axios.post('http://localhost:3001/conversations', {cookies})
         .then(res => {
             setConversations(res.data.conversations)
+        })
+    }
+
+    const getUsers = async () => {
+        axios.post('http://localhost:3001/users', {cookies})
+        .then(res => {
+            console.log(res.data.users)  
+            setUsers(res.data.users)
         })
     }
 
@@ -44,10 +59,14 @@ const Home = () => {
 
     const handleSearch = (e : React.ChangeEvent<HTMLInputElement>) => {
         setSearch(e.target.value)
-        console.log(search)
+    }
+
+    const handleSearchUsers = (e : React.ChangeEvent<HTMLInputElement>) => {
+        setSearchUsers(e.target.value)
     }
 
     const handleNewConv = () => {
+        getUsers()
         setShowNewConv(!showNewConv)
         setCanRotate(true)
         rotateForSec()
@@ -113,14 +132,24 @@ const Home = () => {
 
                 {showNewConv ? (
                     <div className="newconvmodal">
-                        <input type="text" placeholder='Nom de votre interlocuteur' className='newconvinput' />
+                        <input type="text" placeholder='Nom de votre interlocuteur' className='newconvinput' onChange={handleSearchUsers} />
+                        {users.length > 0 ? (
+                            users.map((user) => {
+                                return (
+                                    user.username.toLowerCase().includes(searchUsers.toLowerCase()) ? (
+                                        <div className="newconvuser" key={user._id}>
+                                            <p>{user.username}</p>
+                                        </div>
+                                    ) : null
+                                )
+                            }
+                            )
+                        ) : null}
                     </div>
                 ) : null}
 
-                <div className={`newconv-div ${canRotate ? 'newConv-rotate' : ''}`} 
-                onClick={() => handleNewConv()}>
-                    <img src={NewConv} alt="newconv"
-                    className='newConv' />
+                <div className={`newconv-div ${canRotate ? 'newConv-rotate' : ''}`} onClick={() => handleNewConv()}>
+                    <img src={NewConv} alt="newconv" className='newConv' />
                 </div>
             </div>
 
