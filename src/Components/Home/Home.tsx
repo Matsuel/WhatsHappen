9eee840cfb6a867863e1cc2ce47971f16d041cdb
Matsuel <matsuel0308@gmail.com>
2024-02-07 09:48:00 +0100

    add search bar

--- src/Components/Home/Home.tsx
+++ src/Components/Home/Home.tsx
@@ -1,9 +1,51 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import Cookies from 'js-cookie';
+// @ts-ignore
+import Search from '../../assets/Search.svg'
 import './Home.css'
 
+interface ConversationInfos {
+    _id: string,
+    users_id: string[]
+}
+
 const Home = () => {
+    const [conversations, setConversations] = useState<ConversationInfos[]>([])
+
+    const cookies = Cookies.get('user')
+    
+    const getConversations = async () => {
+        axios.post('http://localhost:3001/conversations', {cookies})
+        .then(res => {
+            setConversations(res.data.conversations)
+        })
+    }
+
+    useEffect(() => {
+        getConversations()
+    }, [])
+
+
     return (
-        <div>
+        <div className="home">
+            <div className="conversations-section">
+                <div className="searchBar">
+                    <img src={Search} alt="search" className='searchLogo' />
+                    <input type="text" placeholder='Rechercher une conversation ici' name="search" id="search" className='convSearch' />
+                </div>
+                {conversations.map((conversation, index) => {
+                    return (
+                        <div key={index} className="conversation">
+                            <p>{conversation._id}</p>
+                        </div>
+                    )
+                })}
+            </div>
+
+            <div className="messages-section">
+
+            </div>
 
         </div>
     )
