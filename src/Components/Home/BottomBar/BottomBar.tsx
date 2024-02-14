3ee746fa972c37c120cdbb1120bc235804558b99
Matsuel <matsuel0308@gmail.com>
2024-02-14 14:38:57 +0100

    delete send button and edit readme

--- src/Components/Home/BottomBar/BottomBar.tsx
+++ src/Components/Home/BottomBar/BottomBar.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import JoinFile from '../../../assets/JoinFile.svg'
-import Send from '../../../assets/Send.svg'
 import VoiceMessage from '../../../assets/VoiceMessage.svg'
 
 import './BottomBar.css'
@@ -29,7 +28,6 @@ const BottomBar = ({ conversationActive , message, handleMessageChange, sendMess
         <div className="conversationbottombar">
             <img src={JoinFile} alt="joinfile" className='joinfile' />
             <input type="text" name="message-input" id="message-input" className='message-input' value={message} onChange={(e) => handleMessageChange(e)} onKeyDown={(e)=> handleEnterPressed(e)} />
-            <img src={Send} alt="send" className='send' onClick={() => sendMessage(conversationActive)} />
             <img src={VoiceMessage} alt="voicemessage" className='voicemessage' />
         </div>
         </div>
