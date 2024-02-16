0744c5b3ac2f1a4e95314bd1389203ddf66c3b37
Matsuel <matsuel0308@gmail.com>
2024-02-16 14:31:53 +0100

    add files input drop zone

--- src/Components/Home/BottomBar/BottomBar.tsx
+++ src/Components/Home/BottomBar/BottomBar.tsx
@@ -1,10 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import JoinFile from '../../../assets/JoinFile.svg'
 import VoiceMessage from '../../../assets/VoiceMessage.svg'
+import Dropzone from 'react-dropzone'
 
 import './BottomBar.css'
 
 const BottomBar = ({ conversationActive , message, handleMessageChange, sendMessage, typingStatus, name }: BottomBarProps) => {
+
+    const [filesInfos, setFilesInfos] = useState<File[]>([])
+    const [filesContent, setFilesContent] = useState<ArrayBuffer[]>([])
+
+    const onDrop = (acceptedFiles: File[]) => {
+        setFilesInfos(prevFilesInfos => [...prevFilesInfos, ...acceptedFiles])
+        acceptedFiles.forEach((file: File) => {
+            const reader = new FileReader()
+            reader.readAsArrayBuffer(file)
+            reader.onload = () => {
+                setFilesContent(prevFilesContent => [...prevFilesContent, reader.result as ArrayBuffer])
+            }
+        })
+    }
 
     const handleEnterPressed = (e: React.KeyboardEvent<HTMLInputElement>) => {
         if (e.key === 'Enter') {
@@ -24,9 +39,31 @@ const BottomBar = ({ conversationActive , message, handleMessageChange, sendMess
             )
             
         }
+
+        <div className="fileslist">
+            {
+                filesInfos.map((file, index) => {
+                    return (
+                        <div key={index} className="file">
+                            <p>{file.name}</p>
+                        </div>
+                    )
+                })
+            }
+        </div>
         
         <div className="conversationbottombar">
-            <img src={JoinFile} alt="joinfile" className='joinfile' />
+            <Dropzone onDrop={(acceptedFiles) => onDrop(acceptedFiles)}>
+                {({getRootProps, getInputProps}) => (
+                    <div className="joinfile" {...getRootProps()}>
+                        <input {...getInputProps()} />
+                        <img src={JoinFile} alt="joinfile" />
+                    </div>
+                )}
+            </Dropzone>
+            <p>{filesContent.length}</p>
+            <p>{filesInfos.length}</p>
+            {/* <img src={JoinFile} alt="joinfile" className='joinfile' /> */}
             <input type="text" name="message-input" id="message-input" className='message-input' value={message} onChange={(e) => handleMessageChange(e)} onKeyDown={(e)=> handleEnterPressed(e)} />
             <img src={VoiceMessage} alt="voicemessage" className='voicemessage' />
         </div>
