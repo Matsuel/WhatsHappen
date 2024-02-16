54a061e15798667057cdecfbfade93192296ee2b
Matsuel <matsuel0308@gmail.com>
2024-02-16 15:14:09 +0100

    v1 of file style

--- src/Components/Home/BottomBar/BottomBar.tsx
+++ src/Components/Home/BottomBar/BottomBar.tsx
@@ -2,17 +2,20 @@ import React, { useState } from 'react'
 import JoinFile from '../../../assets/JoinFile.svg'
 import VoiceMessage from '../../../assets/VoiceMessage.svg'
 import Dropzone from 'react-dropzone'
+import { FileIcon, defaultStyles } from 'react-file-icon'
 
 import './BottomBar.css'
 
-const BottomBar = ({ conversationActive , message, handleMessageChange, sendMessage, typingStatus, name }: BottomBarProps) => {
+const BottomBar = ({ conversationActive, message, handleMessageChange, sendMessage, typingStatus, name }: BottomBarProps) => {
 
     const [filesInfos, setFilesInfos] = useState<File[]>([])
     const [filesContent, setFilesContent] = useState<ArrayBuffer[]>([])
+    const [filesExensions, setFilesExtensions] = useState<string[]>([])
 
     const onDrop = (acceptedFiles: File[]) => {
         setFilesInfos(prevFilesInfos => [...prevFilesInfos, ...acceptedFiles])
         acceptedFiles.forEach((file: File) => {
+            setFilesExtensions(prevFilesExtensions => [...prevFilesExtensions, file.name.split('.').pop() as string])
             const reader = new FileReader()
             reader.readAsArrayBuffer(file)
             reader.onload = () => {
@@ -29,44 +32,49 @@ const BottomBar = ({ conversationActive , message, handleMessageChange, sendMess
 
     return (
         <div className='bottombar'>
-        {
-            typingStatus[conversationActive as keyof typeof typingStatus] ? (
-                <div className="typingstatus">
-                    <p>{name.charAt(0).toUpperCase() + name.slice(1)} est en train d'écrire</p>
-                </div>
-            ) : (
-                null
-            )
-            
-        }
-
-        <div className="fileslist">
             {
-                filesInfos.map((file, index) => {
-                    return (
-                        <div key={index} className="file">
-                            <p>{file.name}</p>
-                        </div>
-                    )
-                })
-            }
-        </div>
-        
-        <div className="conversationbottombar">
-            <Dropzone onDrop={(acceptedFiles) => onDrop(acceptedFiles)}>
-                {({getRootProps, getInputProps}) => (
-                    <div className="joinfile" {...getRootProps()}>
-                        <input {...getInputProps()} />
-                        <img src={JoinFile} alt="joinfile" />
+                typingStatus[conversationActive as keyof typeof typingStatus] ? (
+                    <div className="typingstatus">
+                        <p>{name.charAt(0).toUpperCase() + name.slice(1)} est en train d'écrire</p>
                     </div>
-                )}
-            </Dropzone>
-            <p>{filesContent.length}</p>
-            <p>{filesInfos.length}</p>
-            {/* <img src={JoinFile} alt="joinfile" className='joinfile' /> */}
-            <input type="text" name="message-input" id="message-input" className='message-input' value={message} onChange={(e) => handleMessageChange(e)} onKeyDown={(e)=> handleEnterPressed(e)} />
-            <img src={VoiceMessage} alt="voicemessage" className='voicemessage' />
-        </div>
+                ) : (
+                    null
+                )
+
+            }
+
+            {filesInfos.length > 0 ? (
+                <div className="fileslist">
+                    {
+                        filesInfos.map((file, index) => {
+                            return (
+                                <div key={index} className="file">
+                                    <p className='filename'>{file.name}</p>
+                                    <div className="fileicon">
+                                        <FileIcon extension={filesExensions[index]} {...defaultStyles[filesExensions[index] as keyof typeof defaultStyles]} />
+                                    </div>
+                                </div>
+                            )
+                        })
+                    }
+                </div>
+            ) : null}
+
+            <div className="conversationbottombar">
+                <Dropzone onDrop={(acceptedFiles) => onDrop(acceptedFiles)}>
+                    {({ getRootProps, getInputProps }) => (
+                        <div className="joinfile" {...getRootProps()}>
+                            <input {...getInputProps()} />
+                            <img src={JoinFile} alt="joinfile" />
+                        </div>
+                    )}
+                </Dropzone>
+                <p>{filesContent.length}</p>
+                <p>{filesInfos.length}</p>
+                {/* <img src={JoinFile} alt="joinfile" className='joinfile' /> */}
+                <input type="text" name="message-input" id="message-input" className='message-input' value={message} onChange={(e) => handleMessageChange(e)} onKeyDown={(e) => handleEnterPressed(e)} />
+                <img src={VoiceMessage} alt="voicemessage" className='voicemessage' />
+            </div>
         </div>
     )
 }
