a157fd5c79e81798433edf3caa7513bf203b6c8c
Matsuel <matsuel0308@gmail.com>
2024-03-10 16:34:42 +0100

    patch messagesList for round message if nextmessage date is different functions

--- src/Functions/MessagesList/MessagesList.ts
+++ src/Functions/MessagesList/MessagesList.ts
@@ -1,13 +1,13 @@
-function isBottomRounded(nextMessage: message|null, sender_id: string): boolean {
+function isBottomRounded(nextMessage: message|null, sender_id: string, messageDate : string): boolean {
     if (nextMessage) {
-        return nextMessage.sender_id !== sender_id;
+        return nextMessage.sender_id !== sender_id || new Date(nextMessage.date).toDateString() !== new Date(messageDate).toDateString();
     }
     return true;
 }
 
-function isTopRounded(previousMessage: message|null, sender_id: string): boolean {
+function isTopRounded(previousMessage: message|null, sender_id: string, messageDate : string): boolean {
     if (previousMessage) {
-        return previousMessage.sender_id !== sender_id;
+        return previousMessage.sender_id !== sender_id || new Date(previousMessage.date).toDateString() !== new Date(messageDate).toDateString();
     }
     return true;
 }
