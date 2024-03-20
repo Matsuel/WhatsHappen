1d9b317060e369337f2490d9425557374995c1ae
Matsuel <matsuel0308@gmail.com>
2024-03-20 10:34:51 +0100

    handle escape key for exit right click menu

--- src/Components/Chat/MessagesArea/MessagesList/Message/Message.tsx
+++ src/Components/Chat/MessagesArea/MessagesList/Message/Message.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, KeyboardEvent } from 'react'
 import Picker, { Emoji } from 'emoji-picker-react'
 import { handleContextMenu, handleMouseDown, handleMouseUp } from '../../../../../Functions/Message/Message'
 
@@ -41,13 +41,20 @@ const Message = ({
     const topClass = isReceived ? (topRounded ? styles.messagereceivedtop : styles.messagereceivedmiddle) : (topRounded ? styles.messagesenttop : styles.messagesentmiddle)
     const bottomClass = isReceived ? (bottomRounded ? styles.messagereceivedbottom : '') : (bottomRounded ? styles.messagesentbottom : '');
 
+    const handleEscape = (e: KeyboardEvent<HTMLDivElement>) => {
+        if (e.key === "Escape") {
+            setRightClick(false)
+        }
+    }
+
     return (
         <>
             <div className={styles.message + " " + firstPlan} ref={i === messagesCount - 1 ? scrollBottomRef : null} onContextMenu={(e) => handleContextMenu(e, setRightClick)}
                 onClick={(e) => e.detail === 2 ? handleReaction(message._id, "2764-fe0f") : null}
                 onMouseDown={() => handleMouseDown(setRightClick, setLongPress)}
                 onMouseUp={() => handleMouseUp(longPress, setLongPress)}
-                onMouseLeave={() => handleMouseUp(longPress, setLongPress)}>
+                onMouseLeave={() => handleMouseUp(longPress, setLongPress)}
+                onKeyDown={handleEscape}>
                 {rightClick && <Picker reactionsDefaultOpen={true} className={styles.reactiondiv + " " + (isReceived ? styles.messagecontextmenureceived : styles.messagecontextmenusent)}
                     onEmojiClick={(emoji: EmojiPickerProps) => { handleReaction(message._id, emoji.unified); setRightClick(false); }}
                 />}
