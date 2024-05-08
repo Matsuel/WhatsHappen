d839f568b4d385e46c785361eb96881c6bf79d07
Matsuel <matsuel0308@gmail.com>
2024-05-08 15:31:24 +0200

    context menu for pinned convs

--- src/Components/PinnedConversations/PinnedConversations.tsx
+++ src/Components/PinnedConversations/PinnedConversations.tsx
@@ -6,6 +6,7 @@ import Pinned from '../../assets/Pinned.svg';
 import Bin from '../../assets/Bin.svg';
 import { socket } from '@/pages/_app';
 import { decodeToken } from 'react-jwt';
+import ContextMenuPin from './ContextMenuPinned';
 
 interface PinnedConversationsProps {
     pinnedConversations: ConversationInfos[],
@@ -93,66 +94,3 @@ const PinnedConversations = ({
 };
 
 export default PinnedConversations;
-
-interface ContextMenuPinProps {
-    conversationId: string,
-    points: { x: number, y: number }
-}
-
-
-const ContextMenuPin = ({
-    conversationId,
-    points
-}: ContextMenuPinProps) => {
-
-    let cookies = ""
-    let userId = ""
-    if (typeof window !== 'undefined') {
-        cookies = localStorage.getItem('user') || ''
-        const token: User | null = decodeToken(cookies)
-        userId = token?.userId as string
-    }
-
-    const handlePinnedConversation = (conversation_id: string) => {
-        socket.emit('pinconversation', { cookies, conversation_id })
-    }
-
-    const items = [
-        {
-            label: 'Détacher',
-            onClick: () => handlePinnedConversation(conversationId),
-            color: '#000000',
-            icon: Pinned
-        },
-        {
-            label: 'Supprimer',
-            onClick: () => console.log('delete'),
-            color: '#FF0000',
-            icon: Bin
-        }
-    ]
-
-    return (
-        <div
-            className={styles.contextMenu}
-            style={{ top: points.y, left: points.x }}
-        >
-            {items.map((item) => (
-                <div
-                    className={styles.contextMenuItem}
-                    onClick={item.onClick}
-                    style={{ color: item.color }}
-                >
-                    <p className={styles.contextMenuLabel}>{item.label}</p>
-                    <Image
-                        src={item.icon}
-                        alt="icon"
-                        className={styles.contextMenuIcon}
-                        width={0}
-                        height={0}
-                    />
-                </div>
-            ))}
-        </div>
-    )
-}
