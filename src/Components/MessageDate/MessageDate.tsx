494c69bd5455e66a4aafe025558823bbc3772e32
Matsuel <matsuel0308@gmail.com>
2024-05-09 18:44:07 +0200

    add lines to dates

--- src/Components/MessageDate/MessageDate.tsx
+++ src/Components/MessageDate/MessageDate.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 
 import styles from './MessageDate.module.css'
 
@@ -7,21 +7,38 @@ interface MessageDateProps {
 }
 
 
-const MessageDate = ({ message }: MessageDateProps) => {
+const MessageDate = ({
+    message
+}: MessageDateProps) => {
+
+
+    const [width, setWidth] = useState<number>(0)
+    const ref = useRef<HTMLDivElement>(null)
+
+    useEffect(() => {
+        setWidth(ref.current?.offsetWidth as number)
+        console.log(ref.current?.offsetWidth)
+    }, [])
+
     const today = new Date(message.date).toDateString() === new Date().toDateString()
     const yesterday = new Date(message.date).toDateString() === new Date(Date.now() - 86400000).toDateString()
-    const formatedDate = new Date(message.date).toLocaleDateString('fr-FR',{ weekday: 'long', day: 'numeric', month: 'long', year: 'numeric'}).split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ')
+    const formatedDate = new Date(message.date).toLocaleDateString('fr-FR', { weekday: 'long', day: 'numeric', month: 'long', year: 'numeric' }).split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ')
 
     return (
         <div className={styles.messageday}>
-            <p>{
-                today ?
-                "Aujourd'hui" :
-                yesterday ?
-                "Hier" :
-                formatedDate
+            <div
+                style={{ width: "calc(50% - " + (width / 2 + 50) + "px)" }}
+                className={styles.messagedayline}></div>
+            <p className={styles.messagedaytext} ref={ref}>
+                {
+                    today ?
+                        "Aujourd'hui" :
+                        yesterday ?
+                            "Hier" :
+                            formatedDate
                 }
             </p>
+            <div style={{ width: "calc(50% - " + (width / 2 + 50) + "px)" }} className={styles.messagedayline}></div>
         </div>
     )
 }
