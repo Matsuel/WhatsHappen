b6bb61bf4dc4064e287103d292536844ef11996d
Matsuel <matsuel0308@gmail.com>
2024-05-14 18:19:13 +0200

    deleted not useds socket events on index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -20,19 +20,8 @@ const Home = () => {
 
     const [userId, setUserId] = useState<string>('')
 
-    //déplacer ça dans conversationsList
-    const [conversations, setConversations] = useState<ConversationInfos[]>([])
-
-
-    //dégager ça dans messagesList et l'utiliser à la place de conv
-    const [conversationMessages, setConversationMessages] = useState<message[]>([])
-
     //dégager ça dans conversationsList
     const [conversationActive, setConversationActive] = useState<string>('')
-
-    //refaire ça en séparant les conversations et les messages conversationInfosActive et messagesConversationActive
-    const [conv, setConv] = useState<conversation>({} as conversation)
-
 
     const [search, setSearch] = useState<string>('')
     const [showNewConv, setShowNewConv] = useState<boolean>(false)
@@ -73,49 +62,25 @@ const Home = () => {
         socket.emit('synchro', { userId: userId })
 
         //enlever ça car y'a déjà une fonction qui fait ça pareil pour le on
-        socket.emit('conversations', { cookies })
-
-        socket.on('conversations', (data) => {
-            if (data.conversations) {
-                setConversations(data.conversations)
-            } else {
-                console.log('Échec de la connexion:', data.error);
-            }
-        });
+        socket.emit('conversations', { cookies })        
 
         
-
-        //deplacer ça dans messagesList
-        socket.on('syncmessages', (data) => {
-            setConversationMessages(data.messages)
-            setConv({ ...conv, messages: data.messages })
-            // voir si on peut pas faire autrement voir pk cpt, ça reçoit plus de messages
-            conversations.forEach((conversation) => {
-                if (conversation._id === data.conversations[0]._id) {
-                    conversation.last_message_content = data.conversations[0].last_message_content
-                    conversation.last_message_date = data.conversations[0].last_message_date
-                    conversation.last_message_sender = data.conversations[0].last_message_sender
-                }
-            })
-            setConversations([...conversations])
-        })
-
-        // pas besoin de ça vu qu'on a la liste des gens connectés
-        socket.on('synchrostatus', (data) => {
-            const statusOther = data.status
-            setConversations((prevConversations) => {
-                return prevConversations.map((conv) => {
-                    return { ...conv, status: statusOther[conv._id] }
-                })
-            })
-
-        })
 
         // pas besoin de ça vu qu'on a la liste des gens connectés
         setInterval(() => {
             socket.emit('synchrostatus', { userId: userId })
         }, 5000)
     }, [])
+
+    //deplacer ça dans messagesList
+    socket.on('syncmessages', (data) => {
+        console.log(data)
+    })
+
+    // pas besoin de ça vu qu'on a la liste des gens connectés
+    socket.on('synchrostatus', (data) => {
+        console.log(data)
+    })
 
     //dégager ça dans conversationsList
     const handleConversationActive = (conversationId: string) => {
@@ -125,15 +90,8 @@ const Home = () => {
         }
         setConversationActive(conversationId)
         socket.emit('conversationmessages', { cookies, conversation_id:conversationId })
-        conv.messages = conversationMessages
-        conv.conversationInfos = conversations.find((conv) => conv._id === conversationId) as ConversationInfos
-        conv.conversationInfos.bottomRounded = true
-        conv.conversationInfos.topRounded = true
     }
-
-    //exporter le socket et déplacer cette fonction dans le modal
     
-
     //dégager ça dans modal
     const handleNewConv = () => {
         if (!clickAwayEffect) {
@@ -162,29 +120,19 @@ const Home = () => {
         })
     }
     
-
     //dégager ça dans messageArea
     const handleSearchConv = () => {
         setShowSearchConv(!showSearchConv)
     }
-
 
     //mettre ça dans messagesList
     const deleteMessage = (message_id: string) => {
         socket.emit('deletemessage', { cookies, message_id, conversationActive })
         socket.on('deletemessage', (data: any) => {
             if (data.deleted) {
-                conv.messages = conv.messages.filter((message) => message._id !== message_id)
-                setConv({ ...conv })
-                setConversationMessages(conv.messages)
             }
         })
     }
-
-    //mettre ça dans messagesList
-    
-
-
 
     return (
         <div className={styles.home}>
