bca54a8338392ede0d13af4576877f001fb29649
Matsuel <matsuel0308@gmail.com>
2024-05-22 12:17:37 +0200

    add clickAway to edit message wrap

--- src/Components/Message/EditMessage.tsx
+++ src/Components/Message/EditMessage.tsx
@@ -1,7 +1,8 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React, { LegacyRef, MutableRefObject, useEffect, useRef, useState } from 'react'
 import styles from './style.module.scss'
 import { socket } from '@/pages/_app'
 import { useCookie } from '@/hooks/useCookie/useCookie'
+import { useClickAway } from '@uidotdev/usehooks'
 
 interface EditMessageProps {
     message: message,
@@ -22,6 +23,13 @@ const EditMessage = ({
     const [newMessage, setNewMessage] = useState<string>(message.content)
     const ref = useRef<HTMLInputElement>(null)
 
+    const refAway = useClickAway((event) => {
+        if (ref.current && ref.current.contains(event.target as Node)) {
+            return
+        }
+        setEditMode(false)
+    }) as LegacyRef<HTMLInputElement>
+
     const editMessage = (e: React.KeyboardEvent<HTMLInputElement>) => {
         if (e.key === "Enter" && newMessage !== message.content && newMessage.length > 0 && newMessage.trim().length > 0) {
             socket.emit('editMessage', { cookies: cookies, message_id: message._id, content: newMessage, conversation_id: conversationActive })
@@ -31,24 +39,26 @@ const EditMessage = ({
 
     useEffect(() => {
         if (editMode) {
-            ref.current?.focus()
+            (refAway as MutableRefObject<HTMLInputElement | null>).current?.focus()
         }
-        console.log(message._id)
     }, [editMode])
 
     return (
-        <input
-            type="text"
-            className={[
-                // styles.message,
-                styles.editMessageInput
-
-            ].join(' ')}
-            value={newMessage}
-            onChange={(e) => setNewMessage(e.target.value)}
+        <div
             ref={ref}
-            onKeyDown={(e) => editMessage(e)}
-        />
+            className={styles.editMessageWrap}
+        >
+            <input
+                type="text"
+                className={[
+                    styles.editMessageInput
+                ].join(' ')}
+                value={newMessage}
+                onChange={(e) => setNewMessage(e.target.value)}
+                ref={refAway}
+                onKeyDown={(e) => editMessage(e)}
+            />
+        </div>
     )
 }
 
